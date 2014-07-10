a4efeb0407e0a75ac9577569081e7329fdd4a076
Kamil Szyc <kamilszyc@gmail.com>
2014-07-10 16:33:24 +1000

    Added bonus solitions to problem 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -13,18 +13,25 @@ end
 
 def translate_word word
 	i=0
+	punctuations = [',','!',':','.',';','?']
+	punct=""
 	vowels = ['i','o','u','e','a']
+	if punctuations.include? word[-1]
+		punct=word[-1] 
+		word=word[0...-1]
+	end
 	/[[:upper:]]/.match(word[0]) ? capitalize = true : capitalize = false
 	while not vowels.include? word[i]
 		if word[i..i+1]=='qu'
 			word=word+word[i..i+1]
 			i+=2
+		
 		else
 			word = word+word[i]
 			i += 1
 		end
 	end
-	word = word[i..-1]+"ay"
+	word = word[i..-1]+"ay"+punct
 	if capitalize
 		return word.capitalize!
 	else
@@ -32,3 +39,4 @@ def translate_word word
 	end
 end
 
+#
